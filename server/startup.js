7cadc1687598e5f956161f9c686fb5cfb085880f
Lingxiao Sun <sunny9732@gmail.com>
2016-02-13 15:23:45 -0500

    fine tune

--- server/startup.js
+++ server/startup.js
@@ -1,31 +1,35 @@
 // start up function that creates entries in the Websites databases.
-Meteor.startup(function () {
-  // code to run on server at startup
-  if (!Websites.findOne()){
-    console.log("No websites yet. Creating starter data.");
-      Websites.insert({
-      title:"Goldsmiths Computing Department",
-      url:"http://www.gold.ac.uk/computing/",
-      description:"This is where this course was developed.",
-      createdOn:new Date()
-    });
-     Websites.insert({
-      title:"University of London",
-      url:"http://www.londoninternational.ac.uk/courses/undergraduate/goldsmiths/bsc-creative-computing-bsc-diploma-work-entry-route",
-      description:"University of London International Programme.",
-      createdOn:new Date()
-    });
-     Websites.insert({
-      title:"Coursera",
-      url:"http://www.coursera.org",
-      description:"Universal access to the world’s best education.",
-      createdOn:new Date()
-    });
-    Websites.insert({
-      title:"Google",
-      url:"http://www.google.com",
-      description:"Popular search engine.",
-      createdOn:new Date()
-    });
-  }
+Meteor.startup(function() {
+	// code to run on server at startup
+	if (!Websites.findOne()) {
+		console.log("No websites yet. Creating starter data.");
+		Websites.insert({
+			title: "Goldsmiths Computing Department",
+			url: "http://www.gold.ac.uk/computing/",
+			description: "This is where this course was developed.",
+			upvote: 10,
+			downvote: 3
+		});
+		Websites.insert({
+			title: "University of London",
+			url: "http://www.londoninternational.ac.uk/courses/undergraduate/goldsmiths/bsc-creative-computing-bsc-diploma-work-entry-route",
+			description: "University of London International Programme.",
+			upvote: 5,
+			downvote: 1
+		});
+		Websites.insert({
+			title: "Coursera",
+			url: "http://www.coursera.org",
+			description: "Universal access to the world’s best education.",
+			upvote: 15,
+			downvote: 0
+		});
+		Websites.insert({
+			title: "Google",
+			url: "http://www.google.com",
+			description: "Popular search engine.",
+			upvote: 12,
+			downvote: 8
+		});
+	}
 });
