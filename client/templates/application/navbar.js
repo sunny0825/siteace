f30e0a176af33db613731d43e4aa139dcd42637b
Lingxiao Sun <sunny9732@gmail.com>
2016-02-21 00:15:48 -0500

    - still need to separate nabber into another template to make sure it can be applied to every page - add sorting feature to easy search - add pagination to easy search

---
+++ client/templates/application/navbar.js
@@ -0,0 +1,72 @@
+Template.navbar.events({
+	'click .dropdown-menu': function(event) {
+		event.stopPropagation();
+	},
+
+	'click .show-signup': function(event) {
+		event.preventDefault();
+		$('.signup-form').toggleClass("show")
+	},
+
+	'submit .login-form': function(event) {
+		event.preventDefault();
+		var $email = $('.loginEmail');
+		var $password = $('.loginPassword');
+		Meteor.loginWithPassword($email.val(), $password.val(), function(err) {
+			if (err) {
+				sAlert.error(err.reason, {
+					effect: "flip",
+					onRouteClose: true
+				})
+			}
+		});
+	},
+
+	'submit .signup-form': function(event) {
+		event.preventDefault();
+		var $email = $('.registerEmail');
+		var $password = $('.registerPassword');
+		Accounts.createUser({
+			email: $email.val(),
+			password: $password.val()
+		}, function(err) {
+			if (err) {
+				$email.val("");
+				$password.val("");
+				sAlert.error(err.reason, {
+					effect: "flip",
+					onRouteClose: true
+				});
+			}
+		});
+		return false;
+	},
+
+	'click .logout': function(event) {
+		event.preventDefault();
+		Meteor.logout();
+	},
+
+	'click .action-add-website': function(event) {
+		if (Users.isLoggedIn()) {
+			$('.websit-form-modal').modal('toggle');
+		} else {
+			sAlert.error("Please login first to add new website.", {
+				effect: "flip",
+				onRouteClose: true
+			})
+		}
+	}
+})
+
+Template.navbar.helpers({
+	loggedInUser: function() {
+		return Meteor.user()["emails"][0]["address"]
+	},
+	websiteIndex: WebsitesIndex,
+
+	inputAttribute: {
+		class: 'form-contorl',
+		placeholder: 'Search...'
+	}
+})
